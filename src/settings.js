263480cdf062a2b1312dde3b1630b95dc387de5f
Batjrgal <batk32474@gmail.com>
2026-08-27 08:14:44 +0800

    Update settings

--- src/settings.js
+++ src/settings.js
@@ -1,20 +1,16 @@
 window._CCSettings = {
-    platform: "web-mobile",
-    groupList: ["default", "fruit", "wall"],
-    collisionMatrix: [
-        [true],
-        [false, true, true],
-        [false, true, false]
-    ],
-    hasResourcesBundle: true,
-    hasStartSceneBundle: false,
-    remoteBundles: [],
-    subpackages: [],
-    launchScene: "db://assets/scene/StartScene.fire",
-    orientation: "",
-    server: "",
-    // PvP/network libs load lazily when entering PvP (faster first paint)
-    jsList: [],
-    bundleVers: {},
-    debug: false
+  platform: "web-mobile",
+  groupList: ["default", "fruit", "wall"],
+  collisionMatrix: [[true], [false, true, true], [false, true, false]],
+  hasResourcesBundle: true,
+  hasStartSceneBundle: false,
+  remoteBundles: [],
+  subpackages: [],
+  launchScene: "db://assets/scene/StartScene.fire",
+  orientation: "",
+  server: "",
+  // PvP/network libs load lazily when entering PvP (faster first paint)
+  jsList: [],
+  bundleVers: {},
+  debug: false,
 };
